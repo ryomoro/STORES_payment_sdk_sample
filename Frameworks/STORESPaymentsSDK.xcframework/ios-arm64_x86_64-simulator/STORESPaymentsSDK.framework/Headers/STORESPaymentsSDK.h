5dee60b1cd962ca31c1fda1c77508ad4e72880a9
Ryota Iwai <ryota@hey.jp>
2023-01-27 13:58:17 +0900

    Update STORESPaymentsSDK.xcframework to v1.1.4

--- Frameworks/STORESPaymentsSDK.xcframework/ios-arm64_x86_64-simulator/STORESPaymentsSDK.framework/Headers/STORESPaymentsSDK.h
+++ Frameworks/STORESPaymentsSDK.xcframework/ios-arm64_x86_64-simulator/STORESPaymentsSDK.framework/Headers/STORESPaymentsSDK.h
@@ -8,15 +8,7 @@
 
 #import <STORESPaymentsSDK/Macros.h>
 #import <STORESPaymentsSDK/NSString+CoineyKitAdditions.h>
-#import <STORESPaymentsSDK/UIImage+CoineyKitAdditions.h>
-#import <STORESPaymentsSDK/cy_printer.h>
 
-#import <STORESPaymentsSDK/CMCommand.h>
-#import <STORESPaymentsSDK/CMResponse.h>
-#import <STORESPaymentsSDK/NSData+TLV.h>
-#import <STORESPaymentsSDK/Constants.h>
-
-#import <STORESPaymentsSDK/CYPrinterTraits.h>
 
 //! Project version number for STORESPaymentsSDK.
 FOUNDATION_EXPORT double STORESPaymentsSDKVersionNumber;
